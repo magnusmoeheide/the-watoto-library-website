f14e7a79c236a2f58f0e4f371eca820dfcf6734a
magnusmoeheide <magnus.heide@code.berlin>
2023-04-12 20:10:21 +0200

    test without deleted signup

--- src/container/auth/AdminLogin.jsx
+++ src/container/auth/AdminLogin.jsx
@@ -29,42 +29,43 @@ const AdminLogin = () => {
     <>
       <main>
         <section>
-          <div>
-            <p> FocusApp </p>
+          <div className="adminLogin">
+            <div className="article">
+              <h1>Admin Login</h1>
 
-            <form>
-              <div>
-                <label htmlFor="email-address">Email address</label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  required
-                  placeholder="Email address"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
+              <form>
+                <div>
+                  <label htmlFor="email-address">Email address</label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    required
+                    placeholder="Email address"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
 
-              <div>
-                <label htmlFor="password">Password</label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  placeholder="Password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
+                <div>
+                  <label htmlFor="password">Password</label>
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
 
-              <div>
-                <button onClick={onLogin}>Login</button>
-              </div>
-            </form>
-
-            <p className="text-sm text-white text-center">
-              No account yet? <NavLink to="/signup">Sign up</NavLink>
-            </p>
+                <div>
+                  <button onClick={onLogin}>Login</button>
+                </div>
+                <p className="text-sm text-white text-center">
+                  No account yet? <NavLink to="/signup">Sign up</NavLink>
+                </p>
+              </form>
+            </div>
           </div>
         </section>
       </main>
